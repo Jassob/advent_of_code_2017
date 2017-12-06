d4f2465f932aaaa0ef36669c3951570ad04393bf
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-12-06 09:10:43 +0100

    Completed the second part of the first day.

--- jassob-haskell/day01.hs
+++ jassob-haskell/day01.hs
@@ -1,27 +1,40 @@
 -- Solution to first day in Advent of Code 2017 (adventofcode.com)
 
 import System.Environment (getArgs)
-import Data.Maybe (maybe)
 import Data.Char (digitToInt, isDigit)
-import Data.List (group, nub)
+
+data Part = P1 | P2
 
 main :: IO ()
 main = do
   args <- parseArgs
-  maybe printUsage go args
+  case args of
+    Just (P1, input) -> go (calcSum (+1)) input
+    Just (P2, input) -> go (calcSum (part2 (length input))) input
+    Nothing          -> printUsage
 
-  where go :: String -> IO ()
-        go arg | checkInput arg = print $ calcSum arg
-               | otherwise      = printUsage
+  where go :: (String -> Int) -> String -> IO ()
+        go f arg | checkInput arg = print $ f arg
+                 | otherwise      = printUsage
+
+        part2 :: Int -> Int -> Int
+        part2 len i = i + (len `div` 2)
 
 -- | Looks at the argument to the program and returns Nothing if it's
 -- not whats expected
-parseArgs :: IO (Maybe String)
+parseArgs :: IO (Maybe (Part, String))
 parseArgs = do
   args <- getArgs
   return $ case args of
-    [input] -> pure input
-    _       -> Nothing
+    ["--part", part, input] -> pure (toPart part, input)
+    ["-p"    , part, input] -> pure (toPart part, input)
+    [input]                 -> pure (P1, input)
+    _                       -> Nothing
+
+  where toPart :: String -> Part
+        toPart "1" = P1
+        toPart "2" = P2
+        toPart _   = error "parseArgs: not a valid part"
 
 -- | Verifies that the input is valid
 checkInput :: String -> Bool
@@ -29,21 +42,31 @@ checkInput = all isDigit
 
 printUsage :: IO ()
 printUsage = putStrLn $ concat
-  [ "Usage: day01 $inputSequence", "\n", "\n"
+  [ "Usage: day01 [OPTIONS] $inputSequence", "\n", "\n"
   , "$inputSequence is the problem input that contains "
   , "a sequence of _only_ digits and day01 calculates the sum "
   , "of every digit that match the next digit in the sequence."
+  , "\n", "\n"
+  , "OPTIONS:", "\n"
+  , "--part, -p", "\t", "1|2", "\t", "Select between part 1 or part 2"
+  , "\n", "If part is not selected part 1 will be assumed."
   ]
 
--- | Calculates the sum of all digits in the list that match their
--- next digit.
-calcSum :: String -> Int
-calcSum (c:cs) = sum $ map digitToInt $ go (c:cs) c
-  where go :: String -> Char -> String
-        go (a:b:cs) fst | a == b    = b : go (b:cs) fst
-                        | otherwise = go (b:cs) fst
-        go [a]      fst | a == fst  = [a]
-                        | otherwise = []
-        go []       _               = []
+-- | Calculate the sum of digit that that matches another digit in a
+-- circular array.
+calcSum :: (Int -> Int) -- ^ Next index
+        -> String       -- ^ Digit sequence
+        -> Int          -- ^ Result
+calcSum indFun digits = sum $ map digitToInt $ go digits 0 (length digits - 1)
+  where go :: String -> Int -> Int -> String
+        go cs i end
+          | i == end && curItem == nextItem = [curItem]
+          | i == end = []
+          | curItem == nextItem = curItem : go cs (i + 1) end
+          | otherwise = go cs (i + 1) end
 
-todo = undefined
+          where curItem = cs !! i
+                nextI = indFun i
+                nextItem = if nextI > end
+                  then cs !! (nextI - end - 1)
+                  else cs !! nextI
