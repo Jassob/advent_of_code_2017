5503a8df6b158cedadd4dae0f912de1991d5024b
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-12-13 18:05:14 +0100

    Fixed the second day.

--- jassob-haskell/day02.hs
+++ jassob-haskell/day02.hs
@@ -1,20 +1,26 @@
 -- | Solution to second day in Advent of Code 2017 (adventofcode.com)
 
 import System.Environment (getArgs)
-import Data.Maybe (catMaybes)
+import Data.Maybe (fromJust, catMaybes)
+import Data.List (sortBy)
 
 data Part = P1 | P2
+  deriving (Eq, Show)
+
+type PartFun = [Maybe Int] -> Maybe Int -> Maybe Int
 
 main :: IO ()
 main = do
-   checkSum <- withArgs go <$> parseArgs
+   checkSum <- withArgs (go [(P1, calcChecksum), (P2, calcDivSum)]) <$> parseArgs
    case checkSum of
-     Nothing       -> printUsage
-     Just c -> print c
+     Nothing -> printUsage
+     Just c  -> print c
 
-   where go :: Part -> [[Maybe Int]] -> Maybe Int
-         go P1 grid = calcChecksum grid
-         go P2 grid = undefined
+   where go :: [(Part, PartFun)] -> Part -> [[Maybe Int]] -> Maybe Int
+         go fs part = foldr (fromJust . lookup part $ fs) (Just 0)
+
+withArgs :: (Part -> [[Maybe Int]] -> Maybe Int) -> Maybe (Part, [[Maybe Int]]) -> Maybe Int
+withArgs f arg = arg >>= uncurry f
 
 parseArgs :: IO (Maybe (Part, [[Maybe Int]]))
 parseArgs = do
@@ -46,17 +52,22 @@ printUsage = putStrLn $ concat
   , "-f", "\t", "FILEPATH", "Path to file containing the problem input."
   ]
 
-
-withArgs :: (Part -> [[Maybe Int]] -> Maybe Int) -> Maybe (Part, [[Maybe Int]]) -> Maybe Int
-withArgs f arg = arg >>= \(p, grid) -> f p grid
-
 parseInput :: String -> Maybe [[Maybe Int]]
-parseInput = pure . filter (not . null) . map ((map (pure . read)) . words) . lines
+parseInput = pure . filter (not . null) . map (map (pure . read) . words) . lines
 
-calcChecksum :: [[Maybe Int]] -> Maybe Int
-calcChecksum = foldr go (Just 0)
-  where go :: [Maybe Int] -> Maybe Int -> Maybe Int
-        go row prevSum = (+) <$> prevSum <*> pure checksum
-          where mx = maximum (catMaybes row)
-                mn = minimum (catMaybes row)
-                checksum = mx - mn
+-- | Part 1
+calcChecksum :: [Maybe Int] -> Maybe Int -> Maybe Int
+calcChecksum row prevSum = (+) <$> prevSum <*> pure checksum
+  where mx = maximum (catMaybes row)
+        mn = minimum (catMaybes row)
+        checksum = mx - mn
+
+-- | Part 2
+calcDivSum :: [Maybe Int] -> Maybe Int -> Maybe Int
+calcDivSum row prevSum = (+) <$> prevSum <*> (findDivisibles . sortBy (flip compare)) row
+  where findDivisibles :: [Maybe Int] -> Maybe Int
+        findDivisibles [] = Nothing
+        findDivisibles (n:ns) =
+          case filter (\d -> (mod <$> n <*> d) == Just 0) ns of
+            []       -> findDivisibles ns
+            (n':_) -> div <$> n <*> n'
