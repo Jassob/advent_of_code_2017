ef7ab7e19e0e97fc8be8f95b1c312f9360e02c14
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-12-21 09:45:26 +0100

    Made day01.hs use Lib.hs

--- jassob-haskell/day01.hs
+++ jassob-haskell/day01.hs
@@ -1,47 +1,24 @@
+{-# LANGUAGE FlexibleInstances#-}
 -- Solution to first day in Advent of Code 2017 (adventofcode.com)
 
 import System.Environment (getArgs)
 import Data.Char (digitToInt, isDigit)
 
-data Part = P1 | P2
+import Lib (Arg(..), Part(..), run_)
 
-main :: IO ()
-main = do
-  args <- parseArgs
-  case args of
-    Just (P1, input) -> go (calcSum (+1)) input
-    Just (P2, input) -> go (calcSum (part2 (length input))) input
-    Nothing          -> printUsage
+instance Arg String where
+  parseInput = pure . filter (/='\n')
+  checkArgs  = all isDigit
 
-  where go :: (String -> Int) -> String -> IO ()
-        go f arg | checkInput arg = print $ f arg
-                 | otherwise      = printUsage
+main = run_ (calcSum (+1)) calcSum' usage
+    where part2 :: Int -> Int -> Int
+          part2 len i = i + (len `div` 2)
 
-        part2 :: Int -> Int -> Int
-        part2 len i = i + (len `div` 2)
+          calcSum' :: String -> Int
+          calcSum' ds = calcSum (part2 (length ds)) ds
 
--- | Looks at the argument to the program and returns Nothing if it's
--- not whats expected
-parseArgs :: IO (Maybe (Part, String))
-parseArgs = do
-  args <- getArgs
-  return $ case args of
-    ["--part", part, input] -> pure (toPart part, input)
-    ["-p"    , part, input] -> pure (toPart part, input)
-    [input]                 -> pure (P1, input)
-    _                       -> Nothing
-
-  where toPart :: String -> Part
-        toPart "1" = P1
-        toPart "2" = P2
-        toPart _   = error "parseArgs: not a valid part"
-
--- | Verifies that the input is valid
-checkInput :: String -> Bool
-checkInput = all isDigit
-
-printUsage :: IO ()
-printUsage = putStrLn $ concat
+usage :: String
+usage = concat
   [ "Usage: day01 [OPTIONS] $inputSequence", "\n", "\n"
   , "$inputSequence is the problem input that contains "
   , "a sequence of _only_ digits and day01 calculates the sum "
