6007ca6102a74b9092c6c54d1cf61473f0838569
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2017-12-25 12:24:11 +0100

    Completed the first part of day 6.

---
+++ jassob-haskell/day06.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE FlexibleInstances #-}
+-- | Solution to sixth day in Advent of Code 2017 (adventofcode.com)
+
+import           Data.Function (on)
+import           Data.Foldable (maximumBy)
+import           Data.Vector (Vector, (!), (//), fromList, indexed)
+import qualified Data.Vector as V
+
+import Lib (Part(..), Arg(..), run_)
+
+type Bank = Int
+type Memory = Vector Bank
+
+instance Arg Memory where
+  parseInput = pure . fromList .  map read . words
+
+main :: IO ()
+main = run_ (part1 []) undefined usage
+
+part1 :: [Memory] -> Memory -> Int
+part1 mems mem
+  | mem `elem` mems = length mems
+  | otherwise = part1 (mem : mems) (reallocate mem)
+
+reallocate :: Memory -> Memory
+reallocate mem = updateMemory mem' 0 blocksPerBank extraBlockIdxs
+  where (mem', (idx, blocks)) = findNextBank mem
+        blocksPerBank = blocks `div` length mem
+        extraBlockIdxs = map (`mod` length mem) [idx+1 .. idx + blocks `mod` length mem]
+
+        updateMemory :: Memory -> Int -> Int -> [Int] -> Memory
+        updateMemory mem idx bpb extrb
+          | idx >= length mem = mem
+          | otherwise = updateMemory mem (idx + 1) bpb extrb
+            // [(idx, mem ! idx + blocks' idx bpb extrb)]
+
+        blocks' :: Int -> Int -> [Int] -> Int
+        blocks' idx blocks extrb | idx `elem` extrb = blocks + 1
+                                 | otherwise = blocks
+
+findNextBank :: Memory -> (Memory, (Int, Bank))
+findNextBank mem = (mem // [(idx, 0)], (idx, blocks))
+  where (idx, blocks) = V.head
+                        . V.filter ((== maximum mem) . snd)
+                        . indexed $ mem
+
+usage :: String
+usage = concat
+  [ "Usage: day06 [OPTIONS] [input]", "\n\n"
+  , "input is the problem input that contains a whitespace separated", "\n"
+  , "list of numbers representing the number of blocks contained in", "\n"
+  , "every memory bank. day06 calculates how many reallocation cycles", "\n"
+  , "are needed to reach a configuration that has already been seen.", "\n\n"
+  , "OPTIONS:", "\n"
+  , "--part, -p", "\t", "1|2", "\t", "Select between part 1 or part 2"
+  , "\n", "\t\t\t\t", "If part is not selected part 1 will be assumed,"
+  , "\n\n"
+  , "-f", "\t", "FILEPATH","\t", "Path to file containing the problem input."
+  ]
