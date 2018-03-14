2c34333e202d39784968268a16a5719c0e59e5af
Jacob Jonsson <jacob.t.jonsson@gmail.com>
2018-03-14 15:20:50 +0100

    Added the directory settings

---
+++ jassob-haskell/.dir-locals.el
@@ -0,0 +1,5 @@
+((haskell-mode
+  . ((haskell-process-type . ghci)
+	 (dante-project-root . "/home/jassob/src/advent_of_code_2017/jassob-haskell")))
+ (haskell-cabal-mode
+  . ((haskell-process-type . ghci))))
